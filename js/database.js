7179ddbf9c41337d3c05f5269059ada5fce73476
focushaventeam-cloud <focushaven.team@gmail.com>
2026-04-08 12:11:55 -0400

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,49 @@
+/**
+ * Base de datos de nombres de jugadores para auto-llenado
+ * @const {Object} DB
+ */
+const DB = {
+    m: [
+        ['Alejandro García',6],['Carlos Rodríguez',5],['Diego Martínez',6],
+        ['Pablo López',4],['Javier González',5],['Luis Hernández',4],
+        ['David Pérez',6],['Sergio Sánchez',5],['Fernando Ruiz',3],
+        ['Miguel Díaz',4],['Roberto Moreno',5],['Antonio Muñoz',3],
+        ['Juan Álvarez',4],['Pedro Romero',5],['Marcos Alonso',3],
+        ['Daniel Gutiérrez',6],['Eduardo Torres',4],['Andrés Ramírez',5],
+        ['Nicolás Flores',3],['Adrián Castro',4],['Iván Ortega',5],
+        ['Víctor Rubio',3],['Óscar Navarro',4],['Hugo Domínguez',6],
+        ['Manuel Vargas',5],['Raúl Ibáñez',4],['Rafael Gil',3],
+        ['Álvaro Jiménez',5],['Gonzalo Cruz',4],['Rubén Reyes',3],
+        ['Tomás Vega',6],['Emilio Suárez',4],['Salvador Molina',5],
+        ['Ángel Blanco',3],['Rodrigo Ramos',4],['Ignacio Santos',5],
+        ['Mateo Herrera',3],['Felipe Mora',4],['Agustín Cabrera',5],
+        ['Bernardo Ríos',3],['César Delgado',4],['Clemente Fuentes',5],
+        ['Damián Ponce',3],['Enrique Salinas',6],['Facundo Vidal',4],
+        ['Gastón Medina',5],['Héctor Guerrero',3],['Isaac Peña',4],
+        ['Jairo Campos',5],['Kevin Espinoza',3]
+    ],
+    f: [
+        ['María García',6],['Ana Rodríguez',5],['Laura Martínez',6],
+        ['Sofía López',4],['Carmen González',5],['Elena Hernández',4],
+        ['Isabel Pérez',6],['Lucía Sánchez',5],['Marta Ruiz',3],
+        ['Paula Díaz',4],['Cristina Moreno',5],['Andrea Muñoz',3],
+        ['Silvia Álvarez',4],['Raquel Romero',5],['Beatriz Alonso',3],
+        ['Natalia Gutiérrez',6],['Patricia Torres',4],['Verónica Ramírez',5],
+        ['Lorena Flores',3],['Miriam Castro',4],['Rebeca Ortega',5],
+        ['Sandra Rubio',3],['Pilar Navarro',4],['Rosa Domínguez',6],
+        ['Teresa Vargas',5],['Irene Ibáñez',4],['Claudia Gil',3],
+        ['Mónica Jiménez',5],['Alicia Cruz',4],['Esther Reyes',3],
+        ['Diana Vega',6],['Nuria Suárez',4],['Viviana Molina',5],
+        ['Valeria Blanco',3],['Ximena Ramos',4],['Yanira Santos',5],
+        ['Zara Herrera',3],['Amber Mora',4],['Brenda Cabrera',5],
+        ['Celia Ríos',3],['Delia Delgado',4],['Elvira Fuentes',5],
+        ['Fátima Ponce',3],['Gloria Salinas',6],['Hilda Vidal',4],
+        ['Ingrid Medina',5],['Jimena Guerrero',3],['Karen Peña',4],
+        ['Lina Campos',5],['Maite Espinoza',3]
+    ]
+};
+
+// Exportar para módulos si es necesario
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = DB;
+}
